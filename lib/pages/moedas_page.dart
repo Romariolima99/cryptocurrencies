30b5731e238ffb92d24ae56d27bb56d41124f017
Romário Lima <devromariolima@gmail.com>
2024-09-01 22:06:47 -0300

    Customizar a App Bar e o Floating Action Button

--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -3,33 +3,47 @@ import 'package:cripto/repositories/moeda_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-
-
-
-
 class MoedasPage extends StatefulWidget {
-
- MoedasPage({super.key});
+  MoedasPage({super.key});
 
   @override
   State<MoedasPage> createState() => _MoedasPageState();
 }
 
 class _MoedasPageState extends State<MoedasPage> {
-
   final tabela = MoedaRepository.tabela;
   NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
   List<Moeda> selecionadas = [];
 
+  appApbarDinamica() {
+    if (selecionadas.isEmpty) {
+      return AppBar(
+        title: const Text('Cripto Moedas'),
+        centerTitle: true, // Centraliza o título
+        backgroundColor: Colors.blue, // Define a cor azul para a AppBar
+      );
+    } else {
+      return AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            setState(() {
+              selecionadas = [];
+            });
+          },
+        ),
+        title: Text('${selecionadas.length} selecionadas'),
+        backgroundColor: Colors.blueGrey[50],
+        elevation: 1,
+        iconTheme: const IconThemeData(color: Colors.black87),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cripto Moedas'),
-        centerTitle: true, // Centraliza o título
-        backgroundColor: Colors.blue, // Define a cor azul para a AppBar
-      ),
+      appBar: appApbarDinamica(),
       body: ListView.separated(
         itemBuilder: (BuildContext context, int moeda) {
           return ListTile(
@@ -37,13 +51,13 @@ class _MoedasPageState extends State<MoedasPage> {
               borderRadius: BorderRadius.all(Radius.circular(12)),
             ),
             leading: (selecionadas.contains(tabela[moeda]))
-            ? const CircleAvatar(
-              child: Icon(Icons.check),
-            )
-            : SizedBox(
-              width: 40,
-              child: Image.asset(tabela[moeda].icone),
-            ),
+                ? const CircleAvatar(
+                    child: Icon(Icons.check),
+                  )
+                : SizedBox(
+                    width: 40,
+                    child: Image.asset(tabela[moeda].icone),
+                  ),
             title: Text(
               tabela[moeda].nome,
               style: const TextStyle(
@@ -51,12 +65,10 @@ class _MoedasPageState extends State<MoedasPage> {
                 fontWeight: FontWeight.w500,
               ),
             ),
-            trailing: Text(real.format(tabela[moeda].preco)
-            ),
+            trailing: Text(real.format(tabela[moeda].preco)),
             selected: selecionadas.contains(tabela[moeda]),
             selectedTileColor: Colors.indigo[50],
-            onLongPress:(){
-
+            onLongPress: () {
               setState(() {
                 (selecionadas.contains(tabela[moeda]))
                     ? selecionadas.remove(tabela[moeda])
@@ -69,6 +81,18 @@ class _MoedasPageState extends State<MoedasPage> {
         separatorBuilder: (_, __) => const Divider(),
         itemCount: tabela.length,
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: selecionadas.isNotEmpty
+          ? FloatingActionButton.extended(
+              onPressed: () {},
+              icon: const Icon(Icons.star),
+              label: const Text('Favoritar',
+                  style: TextStyle(
+                    letterSpacing: 0,
+                    fontWeight: FontWeight.bold,
+                  )),
+            )
+          : null,
     );
   }
 }
