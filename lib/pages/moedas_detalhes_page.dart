4de00dd11ded803530cd0f3a7de383260781a09b
Romário Lima <devromariolima@gmail.com>
2024-09-03 21:58:03 -0300

    adição dos dados das cripto moedas

--- lib/pages/moedas_detalhes_page.dart
+++ lib/pages/moedas_detalhes_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:cripto/models/moedas.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class MoedasDetalhesPage extends StatefulWidget {
 
@@ -18,22 +19,41 @@ class MoedasDetalhesPage extends StatefulWidget {
 }
 
 class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
+NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.moeda.nome),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              SizedBox(child: Image.asset(widget.moeda.icone),
-              width: 50,
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(width: 50,child: Image.asset(widget.moeda.icone),
+                  ),
+                  Container(width: 10),
+                  Text(
+                    real.format(widget.moeda.preco),
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: -1,
+                      color: Colors.grey[800],
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
