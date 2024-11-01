ca789b89cf573a5b0a7bc46fb5c28cec23b8b486
Romário Lima <devromariolima@gmail.com>
2024-11-01 17:38:48 -0400

    Add: Firestore / Fix: Firestore

--- lib/repositories/favoritas_repository.dart
+++ lib/repositories/favoritas_repository.dart
@@ -1,22 +1,68 @@
 import 'dart:collection';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cripto/database/db_firestore.dart';
 import 'package:cripto/models/moedas.dart';
+import 'package:cripto/repositories/moeda_repository.dart';
+import 'package:cripto/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class FavoritasRepository extends ChangeNotifier {
   final List<Moeda> _lista = [];
+  late FirebaseFirestore db;
+  late AuthService auth;
+
+  FavoritasRepository({required this.auth}) {
+    _startRepository();
+  }
+
+  _startRepository() async {
+    await _startFirestore();
+    await _readFavoritas();
+  }
+
+  _startFirestore() {
+    db = DbFirestore.get();
+  }
+
+  _readFavoritas() async {
+    if (auth.user != null && _lista.isEmpty) {
+      final snapshot =
+          await db.collection('products/${auth.user!.uid}/favoritas').get();
+      snapshot.docs.forEach((doc) {
+        Moeda moeda = MoedaRepository.tabela
+            .firstWhere((moeda) => moeda.sigla == doc.get('sigla'));
+        _lista.add(moeda);
+        notifyListeners();
+      });
+    }
+  }
 
   UnmodifiableListView<Moeda> get lista => UnmodifiableListView(_lista);
 
   saveAll(List<Moeda> moedas) {
-    moedas.forEach((moeda) {
-      if (!_lista.contains(moeda)) _lista.add(moeda);
+    moedas.forEach((moeda) async {
+      if (!_lista.any((atual) => atual.sigla == moeda.sigla)) {
+        _lista.add(moeda);
+        await db
+            .collection('products/${auth.user!.uid}/favoritas')
+            .doc(moeda.sigla)
+            .set({
+          'moeda': moeda.nome,
+          'sigla': moeda.sigla,
+          'preco': moeda.preco,
+        });
+      }
     });
+
     notifyListeners();
   }
 
-
-  remove(Moeda moeda){
+  remove(Moeda moeda) async {
+    await db
+        .collection('products/${auth.user!.uid}/favoritas')
+        .doc(moeda.sigla)
+        .delete();
     _lista.remove(moeda);
-     notifyListeners();
+    notifyListeners();
   }
 }
