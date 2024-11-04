20783081e51107afe929221a69e41804b7f1bff4
Romário Lima <devromariolima@gmail.com>
2024-11-04 18:18:09 -0400

    Fix: Background LoginPage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,6 +15,126 @@ class _LoginPageState extends State<LoginPage> {
   final email = TextEditingController();
   final senha = TextEditingController();
 
+  Widget _body() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 100),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                titulo,
+                style: const TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.5,
+                  color: Colors.white, // Cor branca para contrastar
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(24),
+                child: TextFormField(
+                  controller: email,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.white), // Cor branca
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Informe o email corretamente!';
+                    }
+                    return null;
+                  },
+                  style: const TextStyle(color: Colors.white), // Cor branca
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 12.0, horizontal: 24.0),
+                child: TextFormField(
+                  controller: senha,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Senha',
+                    labelStyle: TextStyle(color: Colors.white), // Cor branca
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Informe a senha';
+                    } else if (value.length < 6) {
+                      return 'Sua senha deve ter no mínimo 6 caracteres';
+                    }
+                    return null;
+                  },
+                  style: const TextStyle(color: Colors.white), // Cor branca
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: SizedBox(
+                  width: 300,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        if (isLogin) {
+                          login();
+                        } else {
+                          registrar();
+                        }
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: (loading)
+                          ? [
+                              const Padding(
+                                padding: EdgeInsets.all(16),
+                                child: SizedBox(
+                                  width: 24,
+                                  height: 24,
+                                  child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              )
+                            ]
+                          : [
+                              const Icon(Icons.check),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  actionButton,
+                                  style: const TextStyle(fontSize: 20),
+                                ),
+                              )
+                            ],
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () => setFormAction(!isLogin),
+                child: Text(
+                  toggleButton,
+                  style: const TextStyle(color: Colors.white), // Cor branca
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   bool isLogin = true;
   late String titulo;
   late String actionButton;
@@ -31,7 +151,7 @@ class _LoginPageState extends State<LoginPage> {
     setState(() {
       isLogin = acao;
       if (isLogin) {
-        titulo = 'Bem vindo';
+        titulo = 'Crypto Currencies';
         actionButton = 'Login';
         toggleButton = 'Ainda não tem conta? Cadastra-se agora!';
       } else {
@@ -69,109 +189,19 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 100),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  titulo,
-                  style: const TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: -1.5,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(24),
-                  child: TextFormField(
-                    controller: email,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Email'),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Informe o email corretamente!';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 12.0, horizontal: 24.0),
-                  child: TextFormField(
-                    controller: senha,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'senha'),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Informe a senha';
-                      } else if (value.length < 6) {
-                        return 'sua senha deve ter no minimo 6 caracteres';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: SizedBox(
-                    width: 300,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          if (isLogin) {
-                            login();
-                          } else {
-                            registrar();
-                          }
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.blue, // Cor do texto branco
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: (loading)
-                            ? [
-                                const Padding(
-                                  padding: EdgeInsets.all(16),
-                                  child: SizedBox(
-                                    width: 24,
-                                    height: 24,
-                                    child: CircularProgressIndicator(
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                )
-                              ]
-                            : [
-                                const Icon(Icons.check),
-                                Padding(
-                                  padding: const EdgeInsets.all(16.0),
-                                  child: Text(
-                                    actionButton,
-                                    style: const TextStyle(fontSize: 20),
-                                  ),
-                                )
-                              ],
-                      ),
-                    ),
-                  ),
-                ),
-                TextButton(
-                    onPressed: () => setFormAction(!isLogin),
-                    child: Text(toggleButton))
-              ],
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'images/background.jpeg',
+              fit: BoxFit.cover,
             ),
           ),
-        ),
+          Container(
+            color: Colors.black.withOpacity(0.5), // Deixa a imagem mais escura
+          ),
+          _body(), // O corpo da página com texto e botões
+        ],
       ),
     );
   }
